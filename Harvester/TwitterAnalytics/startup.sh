0aa8bca0bdbe3fabbe5d6130717a7f530fd67374
MOSES SATYAM <msatyam@student.unimelb.edu.au>
2018-05-14 21:44:06 +1000

    Signed-off-by: MOSES SATYAM <msatyam@student.unimelb.edu.au> github id mosesspurgeon

--- Harvester/TwitterAnalytics/startup.sh
+++ Harvester/TwitterAnalytics/startup.sh
@@ -4,4 +4,5 @@ java -Xms512M -Xmx768M -Xss16M -Djava.util.logging.config.file=logging.propertie
 java -Xms512M -Xmx768M -Xss16M -Djava.util.logging.config.file=logging.properties -cp bin/:lib/* au.edu.unimelb.comp90024.TwitterAnalytics config/configTweetFileCouch1.properties >logs/TweetFileCouch1.log 2>logs/TweetFileCouch1.error &
 java -Xms512M -Xmx768M -Xss16M -Djava.util.logging.config.file=logging.properties -cp bin/:lib/* au.edu.unimelb.comp90024.TwitterAnalytics config/configTweetSearchFileMel1.properties >logs/TweetSearchFileMel1.log 2>logs/TweetSearchFileMel1.error &
 java -Xms512M -Xmx768M -Xss16M -Djava.util.logging.config.file=logging.properties -cp bin/:lib/* au.edu.unimelb.comp90024.TwitterAnalytics config/configTweetSearchFileSyd1.properties >logs/TweetSearchFileSyd1.log 2>logs/TweetSearchFileSyd1.error &
+#java -Xms512M -Xmx768M -Xss16M -Djava.util.logging.config.file=logging.properties -cp bin/:lib/* au.edu.unimelb.comp90024.TwitterAnalytics config/configTweetFileFile1.properties >logs/TweetFileFile1.log 2>logs/TweetFileFile1.error &
 echo "java application stack started"
---
+++ Harvester/TwitterAnalytics/status.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+ps -ef|grep -i java|grep "TwitterAnalytics"|grep "1.properties"
